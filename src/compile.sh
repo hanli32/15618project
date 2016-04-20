362d485cc578082b1966ad9b02cdd87b1bcab888
Bo Feng <fengbo@fengbos-MBP.wv.cc.cmu.edu>
2016-04-19 21:53:26 -0400

    makefile

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,1 @@
-# nvcc -o test.o -c test.cu
-# nvcc -o test test.o serial_cg.o -lcublas_static -lcusparse_static -lculibos
 nvcc -g -o test test.cu serial_cg.cpp -lcublas_static -lcusparse_static -lculibos
