b53bddfe15fa8a640b89f09d2e5b9792130baa9e
Bo Feng <fengbo@fengbos-MBP.wv.cc.cmu.edu>
2016-05-08 22:37:33 -0400

    add test script

---
+++ src/cuda_cg/experiment_cuda/cuda_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/home/bof/15618project/src/cuda_cg/gpu_test -i /home/bof/DATA/shallow_water2.mtx > shallow_water2_gpu
+/home/bof/15618project/src/cuda_cg/gpu_test -i /home/bof/DATA/ex5.mtx > ex5_gpu
+/home/bof/15618project/src/cuda_cg/gpu_test -i /home/bof/DATA/bcsstm09.mtx > bcsstm09_gpu
+/home/bof/15618project/src/cuda_cg/gpu_test -i /home/bof/DATA/msc10848.mtx > msc10848_gpu
+/home/bof/15618project/src/cuda_cg/gpu_test -i /home/bof/DATA/finan512.mtx > finan512_gpu
+/home/bof/15618project/src/cuda_cg/gpu_test -i /home/bof/DATA/shallow_water2.mtx > shallow_water2_gpu
