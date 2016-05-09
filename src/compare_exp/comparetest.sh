6bf9f4d870d9ee7f72363779c472ccc73697300a
Bo Feng <fengbo@fengbos-MBP.wv.cc.cmu.edu>
2016-05-08 22:45:13 -0400

    add test script

---
+++ src/compare_exp/comparetest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/home/bof/15618project/src/compare_test -i /home/bof/DATA/shallow_water2.mtx > shallow_water2_compare
+/home/bof/15618project/src/compare_test -i /home/bof/DATA/ex5.mtx > ex5_compare
+/home/bof/15618project/src/compare_test -i /home/bof/DATA/bcsstm09.mtx > bcsstm09_compare
+/home/bof/15618project/src/compare_test -i /home/bof/DATA/msc10848.mtx > msc10848_compare
+/home/bof/15618project/src/compare_test -i /home/bof/DATA/finan512.mtx > finan512_compare
+/home/bof/15618project/src/compare_test -i /home/bof/DATA/sts4098.mtx > sts4098_compare
